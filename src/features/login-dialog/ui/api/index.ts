6829e5d776889d430f931e7e812b20f1ffcc8a5f
Sapozhnik <yasapozhnik@gmail.com>
2025-04-09 03:25:33 +0900

    delete login

--- src/features/login-dialog/ui/api/index.ts
+++
@@ -1,9 +0,0 @@
-import instance from '@/shared/api'
-
-export const loginUser = async (userData: {
-  email: string
-  password: string
-}) => {
-  const response = await instance.post('/users/login', userData)
-  return response
-}
